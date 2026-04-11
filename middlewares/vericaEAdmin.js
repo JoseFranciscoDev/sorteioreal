15d258a054058a23b0693d70076b3b5ce2e98cf3
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-04-11 11:13:21 -0300

    refact: remove desvio de fluxo desnecessario no middleware verificaEAdmin

--- middlewares/vericaEAdmin.js
+++ middlewares/vericaEAdmin.js
@@ -2,11 +2,7 @@ const { AUTORIZACAO, BASE_URL } = require("../configs.json");
 const NavBar = require("../utilitarios/NavBar");
 
 function vericaEAdmin(req, res, next) {
-    if (!req.session.usuario || !req.session.usuario.tipo) {
-        const modulos = NavBar.getModulos()
-        return res.render("home.html", { "mensagem": "Você não tem permissão para acessar esta página", "modulos": modulos, BASE_URL });
-    }
-    if (req.session.usuario.tipo != AUTORIZACAO.admin) {
+    if (!req.session.usuario || req.session.usuario.tipo != AUTORIZACAO.admin) {
         const modulos = NavBar.getModulos()
         return res.render("home.html", { "mensagem": "Você não tem permissão para acessar esta página", "modulos": modulos, BASE_URL });
     }
