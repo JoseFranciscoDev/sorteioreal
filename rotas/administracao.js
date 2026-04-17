8468790a4fb186dc4f9e3e02bae111d214d3a20e
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-04-17 14:12:38 -0300

    feat: permite que todos os usuairos possam ver os cupons impressos

--- rotas/administracao.js
+++ rotas/administracao.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Administracao = require("../controle/Administracao.js");
 const Home = require("../controle/Home.js");
+const vericaEAdmin = require("../middlewares/vericaEAdmin.js")
+
 
 router.get("/", Administracao.index);
-router.get("/usuarios", Administracao.listarUsuarios);
+router.get("/usuarios", vericaEAdmin, Administracao.listarUsuarios);
 router.get("/pedidos", Administracao.listarPedidos);
 router.get("/usuarios/criar", Home.criarUsuario);
 router.post("/usuarios/criar", Home.criarUsuarioPost);
