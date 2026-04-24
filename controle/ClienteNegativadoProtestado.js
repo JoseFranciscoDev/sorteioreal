385a0e2712de498d7583b58f5c7eab4692997bb5
mateusmirandapereira <mateus.pereira@ifchat.tech>
2026-04-24 13:10:22 -0300

    implementado a classe data

--- controle/ClienteNegativadoProtestado.js
+++ controle/ClienteNegativadoProtestado.js
@@ -1,4 +1,6 @@
-const lerArquvioUniversal = require("../utilitarios/LerArquivo.js");
+const {lerArquvioUniversal} = require("../utilitarios/LerArquivo.js");
+const ClienteSpc = require("../modelo/ClienteSpc.js");
+const Data = require("../utilitarios/Data.js")
 
 class ClienteNegativadoProtestado {
 
@@ -9,6 +11,39 @@ class ClienteNegativadoProtestado {
         
         const [arquivoUrl] = req.files;
       const dados = await  lerArquvioUniversal(arquivoUrl.path);
+
+      
+
+      const clienteSpc = dados.map(cliente => {
+
+        return new ClienteSpc(
+          cliente.codigo_spc,
+          cliente.cpf_cnpj,
+          cliente.consumidor,
+          cliente.contrato,
+          cliente.data_vencimento,
+          cliente.valor_debito,
+          cliente.data_inclusao,
+          cliente.hora_inclusao,
+          cliente.data_exclusao,
+          cliente.tipo_notificacao,
+          cliente.codigo_notificacao,
+          cliente.cod_associado); 
+
+      });
+
+
+     clienteSpc.forEach(cliente => {
+
+          console.log(`codigoSpc: ${cliente.codigoSpc} | cpfCnpj: ${cliente.cpfCnpj} | consumidor: ${cliente.consumidor} | contrato: ${cliente.contrato} |
+            dataVencimento: ${Data.dataParaTexto(cliente.dataVencimento)} | valorDebito: ${cliente.valorDebito} | dataInclusao: ${Data.dataParaTexto(cliente.dataInclusao)} |
+            horaInclusao: ${cliente.horaInclusao} | dataExclusao: ${Data.dataParaTexto(cliente.dataExclusao)} | tipoNotificacao: ${cliente.tipoNotificacao} |
+            codigoNotificacao: ${cliente.codigoNotificacao} | codAssociado: ${cliente.codAssociado}`);
+      });
+
+
+
+
 
 
       return res.json({dados});
--- modelo/ClienteSpc.js
+++ modelo/ClienteSpc.js
@@ -1,11 +1,82 @@
-
+const Data = require("../utilitarios/Data.js");
 
 class ClienteSpc {
 
     constructor(codigoSpc, cpfCnpj, consumidor, contrato, dataVencimento, valorDebito, dataInclusao, horaInclusao,
-        dataExclusao) {
+        dataExclusao, tipoNotificacao, codigoNotificacao, codAssociado) {
+
+
+        this._codigoSpc = this._textoParaValorInteiro(codigoSpc);
+        this._cpfCnpj = cpfCnpj;
+        this._consumidor = consumidor;
+        this._contrato = contrato;
+        this._dataVencimento = Data.textoParaData(dataVencimento);
+        this._valorDebito = this._textoParaValorDouble(valorDebito);
+        this._dataInclusao = Data.textoParaData(dataInclusao);
+        this._horaInclusao = horaInclusao;
+        this._dataExclusao = Data.textoParaData(dataExclusao);
+        this._tipoNotificacao = tipoNotificacao;
+        this._codigoNotificacao = this._textoParaValorInteiro(codigoNotificacao);
+        this._codAssociado = this._textoParaValorInteiro(codAssociado);
 
         }
+
+
+    get codigoSpc() {
+        return this._codigoSpc;
+    }
+
+    get cpfCnpj() {
+        return this._cpfCnpj
+    }
+
+    get consumidor() {
+        return this._consumidor;
+    }
+
+    get contrato() {
+        return this._contrato
+    }
+
+    get dataVencimento() {
+        return this._dataVencimento;
+    }
+
+    get valorDebito() {
+        return this._valorDebito;
+    }
+
+    get dataInclusao() {
+        return this._dataInclusao;
+    }
+
+    get horaInclusao() {
+        return this._horaInclusao;
+    }
+
+    get dataExclusao() {
+        return this._dataExclusao;
+    }
+
+    get tipoNotificacao() {
+        return this._tipoNotificacao;
+    }
+
+    get codigoNotificacao() {
+        return this._codigoNotificacao;
+    }
+
+    get codAssociado() {
+        return this._codAssociado;
+    }
+
+    _textoParaValorDouble(texto) {
+        return parseFloat(texto);
+    }
+
+    _textoParaValorInteiro(texto) {
+        return parseInt(texto);
+    }
         
 }
 
