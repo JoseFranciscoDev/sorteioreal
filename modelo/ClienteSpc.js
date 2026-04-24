2e6655fc8028165fad4dc1749ecbb59b829f1208
mateusmirandapereira <mateus.pereira@ifchat.tech>
2026-04-24 09:02:57 -0300

    implementado a funcionalidade de remover arquivo

--- modelo/ClienteSpc.js
+++ modelo/ClienteSpc.js
@@ -3,6 +3,12 @@
 class ClienteSpc {
 
     constructor(codigoSpc, cpfCnpj, consumidor, contrato, dataVencimento, valorDebito, dataInclusao, horaInclusao,
-        dataExclusao
-    )
+        dataExclusao) {
+
+        }
+        
 }
+
+
+
+module.exports = ClienteSpc;
