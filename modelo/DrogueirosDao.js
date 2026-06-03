60977b0344fa210fd20c9efb915427d342f42502
JoseFranciscoDev <josefranciscodevpi@gmail.com>
2026-06-03 16:51:23 -0300

    feat: permite a busca de produtos na pagina do drogueiros

--- modelo/DrogueirosDao.js
+++ modelo/DrogueirosDao.js
@@ -4,10 +4,11 @@ const PRODUTOS_POR_PAGINA = 50;
 
 class DrogueirosDao extends AbstractNerusAWS {
 
-    static async getProdutos(limit, offset) {
+    static async getProdutos(limit, offset, busca = null) {
         const conn = await this.connection();
-
-        const sql = `
+        let sql;
+        if (!busca) {
+            sql = `
             SELECT
                 prd.no   AS "Código",
                 prdnam.name   AS "Descrição",
@@ -21,7 +22,7 @@ class DrogueirosDao extends AbstractNerusAWS {
                     WHERE iprd.prdno = prd.no
                     ORDER BY date DESC
                     LIMIT 1
-                )   AS "Ultima Compra"
+                    )   AS "Ultima Compra"
             FROM prd
             JOIN prdnam ON prdnam.prdno = prd.no
             JOIN cl     ON cl.no = prd.clno
@@ -30,10 +31,69 @@ class DrogueirosDao extends AbstractNerusAWS {
             JOIN vend   ON vend.no = inv.vendno
             GROUP BY prd.no
             LIMIT ? OFFSET ?
-        `;
-
-        const [produtos] = await conn.query(sql, [limit, offset]);
-        return produtos;
+                `;
+            const [produtos] = await conn.query(sql, [limit, offset]);
+            return produtos;
+        } if (busca) {
+            const eNumero = !Number.isNaN(Number(busca))
+            if (eNumero) {
+                busca = Number.parseInt(busca)
+                sql = `
+                SELECT
+                    prd.no   AS "Código",
+                    prdnam.name   AS "Descrição",
+                    prd.barcode   AS "Código de Barras",
+                    prd.clno   AS "N. Cent. de Lucro",
+                    cl.name   AS "Nome Cent. Lucro",
+                    vend.name   AS "Fornecedor",
+                    (
+                        SELECT DATE_FORMAT(date, "%d/%m/%Y")
+                        FROM iprd
+                        WHERE iprd.prdno = prd.no
+                        ORDER BY date DESC
+                        LIMIT 1
+                        )   AS "Ultima Compra"
+                FROM prd
+                JOIN prdnam  ON prdnam.prdno = prd.no
+                JOIN cl      ON cl.no = prd.clno
+                LEFT JOIN iprd ON iprd.prdno = prd.no
+                LEFT JOIN inv  ON inv.invno = iprd.invno
+                LEFT JOIN vend ON vend.no = inv.vendno
+                WHERE prd.no = ?
+                GROUP BY prd.no
+                LIMIT ? OFFSET ?`
+                const [produtos] = await conn.query(sql, [busca, limit, offset]);
+                return produtos;
+            }
+            else {
+                sql = `
+                SELECT
+                prd.no   AS "Código",
+                prdnam.name   AS "Descrição",
+                prd.barcode   AS "Código de Barras",
+                prd.clno   AS "N. Cent. de Lucro",
+                cl.name   AS "Nome Cent. Lucro",
+                vend.name   AS "Fornecedor",
+                (
+                    SELECT DATE_FORMAT(date, "%d/%m/%Y")
+                    FROM iprd
+                    WHERE iprd.prdno = prd.no
+                    ORDER BY date DESC
+                    LIMIT 1
+                    )   AS "Ultima Compra"
+                    FROM prd
+                    JOIN prdnam ON prdnam.prdno = prd.no
+                    JOIN cl     ON cl.no = prd.clno
+                    JOIN iprd   ON iprd.prdno = prd.no
+                    JOIN inv    ON inv.invno = iprd.invno
+                    JOIN vend   ON vend.no = inv.vendno
+                    WHERE prdnam.name LIKE ?
+                    GROUP BY prd.no
+                    LIMIT ? OFFSET ?`
+                const [produtos] = await conn.query(sql, [`%${busca}%`, limit, offset]);
+                return produtos;
+            }
+        }
     }
 }
 
